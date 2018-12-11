15267257d92cee3acfd337b862bea76bc3410575
Łukasz Król <lionel@o2.pl>
2018-12-11 19:14:38 +0100

    Add functions for all buttons, compare the choices and random number

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ var newGame = document.getElementById("new-game-button");
 var paperButton = document.getElementById("paper");
 var rockButton = document.getElementById("rock");
 var scissorsButton = document.getElementById("scissors");
+
+var userPoints = document.getElementById("user-score");
+var computerPoints = document.getElementById("computer-score");
+var playerScore = 0;
+var computerScore = 0;
+var rounds;
+
 var roundsInfo = document.getElementById("number-of-rounds");
 var result = document.getElementById("output-result");
+var gameResult = document.getElementById("game-result");
 
 // Functions
 
 function getRounds() { // pobranie i zwrócenie ilości rund do rozegrania
-    var rounds = (window.prompt("Podaj liczbę rund, jaką chcesz rozegrać:"));
+    rounds = (window.prompt("Podaj liczbę rund, jaką chcesz rozegrać:"));
     return rounds;
 };
 
@@ -28,4 +36,60 @@ newGame.addEventListener("click", function() { // wywołanie funkcji
         }   else {
             printRounds(rounds);
         }
+});
+
+// Powyżej działa i jest ok, poniżej nowe funkcje
+
+function getComputerChoice() { // Funkcja losowania "przycisku" przez komputer
+    var result = Math.floor(Math.random() * 3); // Funkcja losująca liczbę, mnożenie przez 3, zaokrąglenie w dół i zapis do zmiennej
+    if (result === 0) { // zamiana liczby na tekst
+        return "papier";
+    } else if (result === 1) {  // zamiana liczby na tekst
+        return "kamień";
+    }   else if (result === 2) {    // zamiana liczby na tekst
+        return "nożyce";
+    }
+};
+
+
+function playerMove(playerChoice) {
+    var computerChoice = getComputerChoice(); // Zapisanie do zmiennej "gotowego" wyboru komputera
+
+    if (playerChoice === computerChoice) { // Porównaie wyników
+        result.innerHTML = "<strong>Remis.</strong> Gracz wybrał" + playerChoice + " - Komputer wybrał " + computerChoice;
+    } else if ((playerChoice === "papier" && computerChoice === "kamień") || (playerChoice === "kamień" && computerChoice === "nożyce") || (playerChoice === "nożyce" && computerChoice === "papier")) {
+            result.innerHTML = "<strong>Wygrana!</strong> Gracz wybrał " + playerChoice + " - Komputer wybrał " + computerChoice;
+            playerScore++;  // Zwiększenie liczby punktów gracza
+            userPoints.innerHTML = playerScore; // Wyświetlenie liczby punktów w tabeli wyników
+            // gameOver(); // Wywołanie funkcji kończącej grę
+            console.log("player score:" + playerScore);
+        }   else {
+            result.innerHTML = "<strong>Przegrana.</strong> Gracz wybrał " + playerChoice + " - Komputer wybrał " + computerChoice;
+            computerScore++;    // Zwiększenie liczby punktów komputera
+            computerPoints.innerHTML = computerScore;   // Wyświetlenie liczby punktów w tabeli wyników
+            // gameOver(); // Wywołanie funkcji kończącej grę
+            console.log("computer score:" + computerScore);
+        }
+        gameOver();
+};
+
+
+function gameOver() { // Funkcja kończąca grę
+    if (playerScore === rounds) { // Porównanie liczby punktów gracza z ilością zadeklarowanych rund
+        gameResult.innerHTML = "Gracz wygrał tą rozgrywkę"; // Wyświetlenie komunikatu o wygranej gracza
+    }   else if (computerScore === rounds) {  // Porównanie liczby punktów komputera z ilością zadeklarowanych rund
+        gameResult.innerHTML = "Komputer wygrał tą rozgrywkę";  // Wyświetlenie komunikatu o wygranej komputera
+    }
+};
+
+paperButton.addEventListener("click", function() {
+    playerMove("papier");
+});
+
+rockButton.addEventListener("click", function() {
+    playerMove("kamień");
+});
+
+scissorsButton.addEventListener("click", function() {
+    playerMove("nożyce");
 });

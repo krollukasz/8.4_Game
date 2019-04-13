fcf74c76f6b9d196a3812115e7a991b4b152590b
Łukasz Król <lionel@o2.pl>
2019-04-14 01:22:26 +0200

    Add scripts for modals but have error

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ var gameResult = document.getElementById("game-result");
 var scoreTable = document.getElementById("scoreTable");
 var userPoints = document.getElementById("user-score");
 var computerPoints = document.getElementById("computer-score");
-var gamePossible = false; // zmienna do zablokowania gry przed podaniem ilości rund
+var tableStats = document.getElementById("table-statistics");
+//var gamePossible = false; // zmienna do zablokowania gry przed podaniem ilości rund
 
 //  ::::: GAME PARAMETERS :::::
 
@@ -20,6 +21,8 @@ var params = {
   playerScore: 0,
   computerScore: 0,
   rounds: 0,
+  playerName: "player",
+  roundWinner: "",
   progress: [],
 };
 
@@ -42,7 +45,7 @@ function gameUnblocked() {
   result.classList.remove("hidden");
   gameResult.classList.remove("hidden");
   scoreTable.classList.remove("hidden");
-  gamePossible = true;
+  // gamePossible = true;
 }
 
 
@@ -53,26 +56,27 @@ function gameUnblocked() {
 
 // ::::: GET ROUND :::::
 
-function getRounds() { // pobranie i zwrócenie ilości rund do rozegrania
-  params.rounds = Number(window.prompt("Podaj liczbę rund, jaką chcesz rozegrać:")); // zapisanie do mniennej wartości typu liczbowego
-  return params.rounds;
-};
+// function getRounds() { // pobranie i zwrócenie ilości rund do rozegrania
+//   params.rounds = Number(window.prompt("Podaj liczbę rund, jaką chcesz rozegrać:")); // zapisanie do mniennej wartości typu liczbowego
+//   return params.rounds;
+// };
 
-function printRounds(rounds) { // wyświetlenie komunikatu z ilością zadeklarowanych rund
-  roundsInfo.innerHTML = "Zadeklarowana ilość rund do wygrania to <strong>" + rounds + "</strong>.";
-};
+// function printRounds(rounds) { // wyświetlenie komunikatu z ilością zadeklarowanych rund
+//   roundsInfo.innerHTML = "Zadeklarowana ilość rund do wygrania to <strong>" + rounds + "</strong>.";
+// };
 
 // ::::: NEW GAME BUTTON :::::
 
-newGame.addEventListener("click", function() { // wywołanie funkcji
-  params.playerScore = 0; // wyzerowanie liczby punktów gracza
-  params.computerScore = 0;  // wyzerowanie liczby punktów komputera
-  userPoints.innerHTML = "0"; // wyzerowanie wyniku
-  computerPoints.innerHTML = "0";  // wyzerowanie wyniku
-  params.rounds = 0; // wyzerowanie liczby rund
-  result.innerHTML = "";  // usunięcie komunikatu
-  gameResult.innerHTML = "";  // usunięcie komunikatu
-  params.rounds = getRounds();
+newGame.addEventListener("click", function() {
+  displayModal("#modal");
+  params.playerName = document.querySelector('[name="name"]').value; // pobrabnie wartości imię z formularza
+  if (params.playerName == "") { // sprawdzam czy podano imię
+    roundsInfo.innerHTML = "To pole nie może być puste";
+    gameBlocked;
+  } else {
+    gameUnblocked();
+  }
+  params.rounds = document.querySelector('[name="rounds"]').value; // pobranie ilości rund z formularza
   if (params.rounds == "" || params.rounds == null) {
     roundsInfo.innerHTML = "Nie podano ilości rund";
     gameBlocked();
@@ -83,7 +87,33 @@ newGame.addEventListener("click", function() { // wywołanie funkcji
     printRounds(params.rounds);
     gameUnblocked();
   }
+  tableStats.innerHTML = ""; // inicjacja tabeli z wynikami (chyba potrzebne ??)
+  showScore(params.playerScore, params.computerScore); // do tabeli wyników
+  roundsInfo.innerHTML = "Zadeklarowana ilość rund do wygrania to <strong>" + params.rounds + "</strong>.";
+  document.getElementById("new-player").innerHTML =
+    "<p>Witaj " + params.playerName + ", baw się dobrze i... powodzenia :)</p>";
 });
+
+// newGame.addEventListener("click", function() { // wywołanie funkcji
+//   params.playerScore = 0; // wyzerowanie liczby punktów gracza
+//   params.computerScore = 0;  // wyzerowanie liczby punktów komputera
+//   userPoints.innerHTML = "0"; // wyzerowanie wyniku
+//   computerPoints.innerHTML = "0";  // wyzerowanie wyniku
+//   params.rounds = 0; // wyzerowanie liczby rund
+//   result.innerHTML = "";  // usunięcie komunikatu
+//   gameResult.innerHTML = "";  // usunięcie komunikatu
+//   params.rounds = getRounds();
+//   if (params.rounds == "" || params.rounds == null) {
+//     roundsInfo.innerHTML = "Nie podano ilości rund";
+//     gameBlocked();
+//   } else if (isNaN(params.rounds)) {
+//     roundsInfo.innerHTML = "Podana wartość nie jest liczbą";
+//     gameBlocked();
+//   } else {
+//     printRounds(params.rounds);
+//     gameUnblocked();
+//   }
+// });
 
 // ::::: COMPUTER CHOICE :::::
 
@@ -111,17 +141,60 @@ document.querySelectorAll('.player-move').forEach(function(element) {
 function playerMove(playerChoice) {
   var computerChoice = getComputerChoice(); // Zapisanie do zmiennej "gotowego" wyboru komputera
   if (playerChoice === computerChoice) { // Porównaie wyników
-    result.innerHTML = "<strong>Remis.</strong> Gracz wybrał" + playerChoice + " - Komputer wybrał " + computerChoice;
+    result.innerHTML = "<strong>Remis.</strong> Gracz wybrał " + playerChoice + " - Komputer wybrał " + computerChoice;
   } else if ((playerChoice === "papier" && computerChoice === "kamień") || (playerChoice === "kamień" && computerChoice === "nożyce") || (playerChoice === "nożyce" && computerChoice === "papier")) {
     result.innerHTML = "<strong>Wygrana!</strong> Gracz wybrał " + playerChoice + " - Komputer wybrał " + computerChoice;
+    roundResult = "Wygrana";
     params.playerScore++;  // Zwiększenie liczby punktów gracza
+    params.roundWinner = "player" // A DA SIĘ TU WSTAWIĆ IMIĘ GRACZA PODANE W INPUCIE ??
     userPoints.innerHTML = params.playerScore; // Wyświetlenie liczby punktów w tabeli wyników
   } else {
     result.innerHTML = "<strong>Przegrana.</strong> Gracz wybrał " + playerChoice + " - Komputer wybrał " + computerChoice;
+    roundResult = "Przegrana"
     params.computerScore++;    // Zwiększenie liczby punktów komputera
+    params.roundWinner = "computer"
     computerPoints.innerHTML = params.computerScore;   // Wyświetlenie liczby punktów w tabeli wyników
   }
+  
+  // przesłanie wyników do tablicy
+  params.progress.push({
+    roundNumber: params.rounds,
+    eachRoundPlayerMove: params.playerChoice,
+    eachRoundComputerMove: params.computerChoice,
+    roundResult: roundResult,
+    gameScore: params.playerScore + " : " + params.computerScore
+  })
+  // sprawdzenie czy ostatnia runda
+  if ((params.playerScore == params.rounds) || (params.computerScore == params.rounds)) {
+    // gameWinner(); // DOPISAĆ FUNKCJĘ NA KONIEC GRY !!!!!!!!!!!!!!
+    showModal("#end-modal"); // uruchomienie modala na koniec gry
+    endGameTable();
+  } else {
+    params.rounds++;
+  }
   gameOver();
+};
+
+// ::::: RESULT TABLE :::::
+// Create table in modal
+
+var endGameTable = function() {
+  tableStats.innerHTML = 
+    "<thead><tr><th>Runda</th><th>Wybór gracza</th><th>Wybór komputera</th><th>Wynik rundy</th><th>Wynik gry</th></tr></thead>";
+  for ( var i = 0; i < params.progress.length; i++ ) {
+    tableStats.innerHTML +=
+    "<tr><td>" +
+    params.progress[i].roundNumber +
+    "</td><td>" +
+    params.progress[i].eachRoundPlayerMove +
+    "</td><td>" +
+    params.progress[i].eachRoundComputerMove +
+    "</td><td>" +
+    params.progress[i].roundResult +
+    "</td><td>" +
+    params.progress[i].gameScore +
+    "</td><tr>";
+  }
 };
 
 // ::::: GAME OVER :::::
@@ -138,6 +211,20 @@ function gameOver() { // Funkcja kończąca grę
     gameBlocked();
     params.gamePossible = false; // zablokowanie dalszej rozgrywki
   }
+};
+
+// ::::: MODALS :::::
+
+var displayModal = function() {
+  document.querySelector("#modal-overlay").classList.add("visible");
+  document.querySelector(".modal").classList.add("visible");
+};
+
+var hideModal = function() {
+  document.querySelectorAll(".modal").forEach(function(modal) {
+    modal.classList.remove("visible");
+  });
+  document.querySelector("#modal-overlay").classList.remove("visible");
 };
 
 // BUTTONS
